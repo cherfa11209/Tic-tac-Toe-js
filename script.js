b6e9b4f23f1cdc19b760e301769dd82981cbb94d
Sherif Cherfa <69639595+cherfa11209@users.noreply.github.com>
2021-01-15 20:33:29 +0000

    Update script.js

--- script.js
+++ script.js
@@ -5,9 +5,10 @@ const turn_count = document.getElementById("turn_count");
 const result =  document.getElementById("result");
 const reset = document.getElementById("reset");
 const gameplay  = prompt("This is a simple game of Tic- Tac- Toe, would you like to play against a player or the PC? 1 for player, 2 for PC (ANY OTHER INPUTS WILL BE CONSIDERED A 1): ")
-let turn = 1; 
+let turn = 0; 
 let player_score = 0;
 let pc_score = 0;
+let emptyCells = []
 
 function updatePlayerScore(){
     p1_score.innerHTML = player_score;
@@ -60,11 +61,13 @@ function checkWinner(){
             result.innerHTML = "Player won this last game!";
             updatePlayerScore();
             clearBoard();
+            turn = 9;
         } else if(cells[0].innerHTML == "x"){
             pc_score++;
             result.innerHTML = "PC won this last game!"
             updatePCscore();
             clearBoard();
+            turn = 9;
         }
     } else if(cells[0].innerHTML == cells[3].innerHTML && cells[3].innerHTML == cells[6].innerHTML && cells[0].innerHTML != "" && cells[3].innerHTML != "" && cells[6].innerHTML != ""){
         if(cells[0].innerHTML == "o"){
@@ -72,11 +75,13 @@ function checkWinner(){
             result.innerHTML = "Player won this last game!";
             updatePlayerScore();
             clearBoard();
+            turn = 9;
         } else if(cells[0].innerHTML == "x"){
             pc_score++;
             result.innerHTML = "PC won this last game!"
             updatePCscore();
             clearBoard();
+            turn = 9;
         }
     } else if(cells[0].innerHTML == cells[4].innerHTML && cells[4].innerHTML == cells[8].innerHTML && cells[0].innerHTML != "" && cells[4].innerHTML != "" && cells[8].innerHTML != ""){
         if(cells[0].innerHTML == "o"){
@@ -84,11 +89,13 @@ function checkWinner(){
             result.innerHTML = "Player won this last game!";
             updatePlayerScore();
             clearBoard();
+            turn = 9;
         } else if(cells[0].innerHTML == "x"){
             pc_score++;
             result.innerHTML = "PC won this last game!"
             updatePCscore();
             clearBoard();
+            turn = 9;
         }
     } else if(cells[2].innerHTML == cells[4].innerHTML && cells[4].innerHTML == cells[6].innerHTML && cells[2].innerHTML != "" && cells[4].innerHTML != "" && cells[6].innerHTML != ""){
         if(cells[2].innerHTML == "o"){
@@ -96,11 +103,13 @@ function checkWinner(){
             result.innerHTML = "Player won this last game!";
             updatePlayerScore();
             clearBoard();
+            turn = 9;
         } else if(cells[2].innerHTML == "x"){
             pc_score++;
             result.innerHTML = "PC won this last game!"
             updatePCscore();
             clearBoard();
+            turn = 9;
         }
     } else if(cells[2].innerHTML == cells[5].innerHTML && cells[5].innerHTML == cells[8].innerHTML && cells[2].innerHTML != "" && cells[5].innerHTML != "" && cells[8].innerHTML != ""){
         if(cells[2].innerHTML == "o"){
@@ -108,11 +117,13 @@ function checkWinner(){
             result.innerHTML = "Player won this last game!";
             updatePlayerScore();
             clearBoard();
+            turn = 9;
         } else if(cells[2].innerHTML == "x"){
             pc_score++;
             result.innerHTML = "PC won this last game!"
             updatePCscore();
             clearBoard();
+            turn = 9;
         }
     } else if(cells[6].innerHTML == cells[7].innerHTML && cells[7].innerHTML == cells[8].innerHTML && cells[6].innerHTML != "" && cells[7].innerHTML != "" && cells[8].innerHTML != ""){
         if(cells[6].innerHTML == "o"){
@@ -120,11 +131,13 @@ function checkWinner(){
             result.innerHTML = "Player won this last game!";
             updatePlayerScore();
             clearBoard();
+            turn = 9;
         } else if(cells[6].innerHTML == "x"){
             pc_score++;
             result.innerHTML = "PC won this last game!"
             updatePCscore();
             clearBoard();
+            turn = 9;
         }
     } else if(cells[3].innerHTML == cells[4].innerHTML && cells[4].innerHTML == cells[5].innerHTML && cells[3].innerHTML != "" && cells[4].innerHTML != "" && cells[5].innerHTML != ""){
         if(cells[3].innerHTML == "o"){
@@ -132,11 +145,13 @@ function checkWinner(){
             result.innerHTML = "Player won this last game!";
             updatePlayerScore();
             clearBoard();
+            turn = 9;
         } else if(cells[3].innerHTML == "x"){
             pc_score++;
             result.innerHTML = "PC won this last game!"
             updatePCscore();
             clearBoard();
+            turn = 9;
         }
     } else if(cells[1].innerHTML == cells[4].innerHTML && cells[4].innerHTML == cells[7].innerHTML && cells[1].innerHTML != "" && cells[4].innerHTML != "" && cells[7].innerHTML != ""){
         if(cells[1].innerHTML == "o"){
@@ -144,11 +159,13 @@ function checkWinner(){
             result.innerHTML = "Player won this last game!";
             updatePlayerScore();
             clearBoard();
+            turn = 9;
         } else if(cells[1].innerHTML == "x"){
             pc_score++;
             result.innerHTML = "PC won this last game!"
             updatePCscore();
             clearBoard();
+            turn = 9;
         }
     } else {
         console.log("next turn!")
@@ -175,14 +192,62 @@ function clearBoard(){
     })
 }
 
-function decidePlayer(){
-    if(gameplay == 1){
-        playerVSplayer();
-    } else if(gameplay == 2){
-        playerVSpc();
+
+function playerVSpc(){
+    cells.forEach((cell) =>{
+        cell.addEventListener("click", function(){
+            while(turn <= 9){
+            if(turn%2 == 0){
+                if(check_availability(cell.innerHTML) == true){
+                    cell.innerHTML = "o";
+                    console.log(cell.innerHTML)
+                    turn++;
+                    checkWinner()
+                    updateTurn()
+                } else {
+                    return 
+                }
+            } else if(turn%2 != 0) {
+                pcTurn();
+                turn++;
+                checkWinner()
+            }
+            }
+        })
+    })
+}
+
+function checkForEmptyCell(){
+    cells.forEach((cell) =>{
+        if(cell.innerHTML == "")
+        emptyCells.push(cell) 
+    })
+}
+
+function pcTurn(){
+    console.log("hi")
+    let pcChoice = Math.floor(Math.random()*9)
+    available = check_availability(cells[pcChoice].innerHTML)
+    if(available == true){
+        cells[pcChoice].innerHTML = "x"
+    } else {
+        checkForEmptyCell();
+        emptyCells[0].innerHTML = "x";
+        console.log(emptyCells)
+        emptyCells.shift()
     }
 }
 
-playerVSplayer()
+function decidePlayer(){
+    if(gameplay == 1){
+        updateTurn()
+        playerVSplayer();
+    } else if(gameplay == 2){
+        updateTurn()
+        playerVSpc()
+    }
+}
+
+decidePlayer();
 
 resetBoard()
